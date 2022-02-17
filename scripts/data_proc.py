0129f169aa508b92c74749ce1dffda4b08533f2d
Dionis Totsila <st1067532@ceid.upatras.gr>
2022-02-17 18:40:37 +0200

    save tf-idf without stemmed

--- scripts/data_proc.py
+++ scripts/data_proc.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+from sklearn.model_selection import train_test_split
+from time import time
 from sklearn.naive_bayes import GaussianNB
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # NLP stuff
-from nltk import word_tokenize
-from nltk.stem import PorterStemmer
-from nltk.corpus import stopwords as sw
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+from utils import processing, has_numbers, has_currency, sms_len
 
 # Read dataset into Pandas-Data Frame  beware of the encoding
 df = pd.read_csv('data/dataset.csv', encoding="ISO-8859-1")
@@ -21,74 +19,45 @@ df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
 
 # Replace the column names so that they are more identifiable and easily accesed
 df = df.rename(columns={"v1": "category", "v2": "text"})
-print(df.head())
-
-# Some plots showing the dataset distribution
-ham_count = len(df[(df["category"] == "ham")])
-spam_count = len(df[(df["category"] == "spam")])
-
-# Dataset Distribution
-fig, ax = plt.subplots()
-
-# Example data
-categories = ['Spam', 'Ham']
-count = [spam_count, ham_count]
-ax.bar(categories, count)
-plt.show()
-# We have an inbalanced dataset 86:14 so either undersampling or downsamplin
-
-# text processing
-
-ps = PorterStemmer()
-stop_words = set(sw.words("english"))
-
-# stem words that are not stop words and are alphabetic strings
-
-
-def processing(row):
-    text = row["text"]
-    tokens = word_tokenize(text)
-    stemmed_tokens = [ps.stem(word.lower()) for word in tokens if (
-        word.isalpha() and word not in stop_words)]
-    joined = (" ".join(stemmed_tokens))
-    return joined
-
 
 # apply processing to every SMS and store results to a new row
-df['processed'] = df.apply(processing, axis = 1)
-df = df.drop(['text'], axis = 1)
+df['processed'] = df.apply(processing, axis=1)
+
+# add a new rowsthat indicate if the message has numerical values
+df['has_num'] = df.apply(has_numbers, axis=1)
+
+# add a new rowsthat indicate if the message has currency related stuff
+df['has_money'] = df.apply(has_currency, axis=1)
+# add a new rows that contain the length of the message
+
+df['length'] = df.apply(sms_len, axis=1)
+df = df.drop(['text'], axis=1)
+
 # storing data frame to csv so that it can be use from future py script
 df.to_pickle('data/stemmed.pkl')
 
 
 # tfidf vector representation of texts
-Transformer = TfidfVectorizer(max_features = 2000)
+Transformer = TfidfVectorizer(max_features=2500)
 tfidf = Transformer.fit_transform(df.processed.values.astype('U'))
 tfidfDF = pd.DataFrame(tfidf.todense())
 tfidfDF.columns = sorted(Transformer.vocabulary_)
-concaten = pd.concat([df, tfidfDF], axis = 1)
-concaten = concaten.drop(['processed'], axis = 1)
-
+concaten = pd.concat([df, tfidfDF], axis=1)
+concaten = concaten.drop(['processed'], axis=1)
+print(concaten)
 concaten.to_pickle(r'data/tfidf.pkl')
 
 
-# split ham and spam
-# rows = 5572
-ham_df = concaten[concaten['category'] == 'ham']
-# rows = 747
-spam_df = concaten[concaten['category'] == 'spam'] 
+# X_train, X_test, Y_train, Y_test = train_test_split(
+#     concaten.iloc[:, 2:], df['category'], test_size=0.2)
 
-# under sample
-
-ham_under_sampled = ham_df[:1000]
-
-under_sampled_df = pd.concat([ham_under_sampled, spam_df])
-under_sampled_df = under_sampled_df.sample(frac=1).reset_index(drop=True)
-under_sampled_df.to_pickle(r'data/under_sampled.pkl')
-
-
-spam_over_sampled = pd.concat([spam_df]*4, ignore_index=True) # Ignores the index
-over_sampled_df = pd.concat([spam_over_sampled, ham_df])
-over_sampled_df = over_sampled_df.sample(frac=1).reset_index(drop=True)
-over_sampled_df.to_pickle(r'data/over_sampled.pkl')
-
+# t0 = time()
+# model = GaussianNB()
+# model.fit(X_train, Y_train)
+# print(f"\nTraining time: {round(time()-t0, 3)}s")
+# t0 = time()
+# score_train = model.score(X_train, Y_train)
+# # print(f"Prediction time (train): {round(time()-t0, 3)}s")t0 = time()
+# score_test = model.score(X_test, Y_test)
+# # print(f"Prediction time (test): {round(time()-t0, 3)}s")print(“\nTrain set score:", score_train)
+# print("Test set score:", score_test)
